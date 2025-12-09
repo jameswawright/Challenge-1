a0dea358826b258f15cf2ce766de0cddafd22a33
jameswawright <jameswawright@icloud.com>
2025-12-09 16:59:19 +0000

    Done

---
+++ Assets/Challenge 1/Scripts/RotatePropeller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RotatePropeller : MonoBehaviour
+{
+    private float rotationSpeed = 1000.0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
